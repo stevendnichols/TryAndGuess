8711b8c2fc2ede22e0c6ff7b13830c31b5a2f17f
stevendnichols <stevendnichols@users.noreply.github.com>
2023-10-25 12:27:54 -0400

    Update guess_what.c
    Minor bug fix

--- guess_what.c
+++ guess_what.c
@@ -3,5 +3,6 @@ int GuessWhatThisDoesWithoutRunningIt(int x, int y);
 int GuessWhatThisDoesWithoutRunningIt(int x, int y)
 {
    int z = (x & y) << 1;
-   return (z ? GuessWhatThisDoesWithoutRunningIt(x^y, z) : x^y;
+   return (z ? GuessWhatThisDoesWithoutRunningIt(x^y, z) : x^y);
 }
+
